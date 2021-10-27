396bc2d4459adbd3a7afe1ce7094480ebcd3f240
isissi <isissi_525@hotmail.com>
2021-10-26 20:28:50 -0600

    initial push

---
+++ map.js
@@ -0,0 +1,39 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const arrays = [];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+const assertArraysEqual = function (arrOne, arrTwo) {
+  if (eqArrays(arrOne, arrTwo) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arrOne} ===  ${arrTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !==  ${arrTwo}`);
+  }
+};
+
+const eqArrays = function (argOne, argTwo) {
+  if (argOne.length !== argTwo.length) {
+    return false;
+  }
+  let i = 0;
+  while (i <= argOne.length) {
+    if (i === argOne.length) {
+      return true;
+    } else if (argOne[i] === argTwo[i]) {
+      i += 1;
+    } else {
+      return false;
+    }
+  }
+};
+
+//const result1 = map(words, word => word[0]);
+const result2 = map(arrays, array => array[0]);
+
+// assertArraysEqual(result1, ['g', 'c', 't', 'm', 't']);
+assertArraysEqual(result2, []);
