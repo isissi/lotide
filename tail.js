f1c8b866d38e2495b9c4994d44e01bea0db4778a
isissi <isissi_525@hotmail.com>
2021-11-07 13:23:16 -0700

    update

--- tail.js
+++ tail.js
@@ -1,21 +1,4 @@
 // FUNCTION IMPLEMENTATTION
-const assertEqual = function(result, arrayTail) {
-  if ((result.length === 0) && (arrayTail.length === 0)) {
-    console.log(`✅✅✅ Assertion Passed: ${result} ===  ${arrayTail}`);
-    return true;
-  } else if (result.length === arrayTail.length) {
-    for (let i = 0; i < result.length; i++) {
-      if (result[i] === arrayTail[i]) {
-        console.log(`✅✅✅ Assertion Passed: ${result} ===  ${arrayTail}`);
-        return true;
-      } else {
-        console.log(`🛑🛑🛑 Assertion Failed: ${result} !==  ${arrayTail}`);
-        return false;
-      }
-    }
-  }
-};
-
 function tail(array) {
   if (array.length === 0) {
     return array;
@@ -26,6 +9,6 @@ function tail(array) {
   }
 }
 
-// TEST CONDE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+
+
+module.exports = tail;
