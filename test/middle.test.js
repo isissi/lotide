4308543cdb19cfdc226dac972541ff454b28c740
isissi <isissi_525@hotmail.com>
2021-11-07 21:13:46 -0700

    update

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,12 +1,32 @@
 const eqArrays = require("../eqArrays");
 const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
+const chai = require('chai');
+const assert = chai.assert;
 
 //test code
-middle([1]); // => []
-middle([1, 2]); // => []
-middle([1, 2, 3, 4]);
+describe("#middle", () => {
+  it("returns [] if the input is [1]]", () => {
+    const actual = middle([1]);
+    const expected = [];
+    assertArraysEqual(actual, expected);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+  it("returns [] if the input is [1, 2]]", () => {
+    const actual = middle([1, 2]);
+    const expected = [];
+    assertArraysEqual(actual, expected);
+  });
+
+  it("returns [2] if the input is [1, 2, 3]]", () => {
+    const actual = middle([1, 2, 3]);
+    const expected = [2];
+    assertArraysEqual(actual, expected);
+  });
+
+  it("returns [2, 3] if the input is [1, 2, 3, 4]]", () => {
+    const actual = middle([1, 2, 3, 4]);
+    const expected = [2, 3];
+    assertArraysEqual(actual, expected);
+  });
+});
