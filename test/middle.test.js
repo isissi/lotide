5c08d528f73f69ae4666afd1fd77ac3aa186655b
isissi <isissi_525@hotmail.com>
2021-11-07 21:20:05 -0700

    update

--- test/middle.test.js
+++ test/middle.test.js
@@ -6,27 +6,27 @@ const assert = chai.assert;
 
 //test code
 describe("#middle", () => {
-  it("returns [] if the input is [1]]", () => {
+  it("returns [] if the input is [1]", () => {
     const actual = middle([1]);
     const expected = [];
-    assertArraysEqual(actual, expected);
+    assert.deepEqual(actual, expected);
   });
 
-  it("returns [] if the input is [1, 2]]", () => {
+  it("returns [] if the input is [1, 2]", () => {
     const actual = middle([1, 2]);
     const expected = [];
-    assertArraysEqual(actual, expected);
+    assert.deepEqual(actual, expected);
   });
 
-  it("returns [2] if the input is [1, 2, 3]]", () => {
+  it("returns [2] if the input is [1, 2, 3]", () => {
     const actual = middle([1, 2, 3]);
     const expected = [2];
-    assertArraysEqual(actual, expected);
+    assert.deepEqual(actual, expected);
   });
 
-  it("returns [2, 3] if the input is [1, 2, 3, 4]]", () => {
+  it("returns [2, 3] if the input is [1, 2, 3, 4]", () => {
     const actual = middle([1, 2, 3, 4]);
     const expected = [2, 3];
-    assertArraysEqual(actual, expected);
+    assert.deepEqual(actual, expected);
   });
 });
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,8 +1,38 @@
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const chai = require('chai');
+const assert = chai.assert;
 
 //test code
 // Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] if the input is ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    const actual = tail(["Yo Yo", "Lighthouse", "Labs"]);
+    const expected = ["Lighthouse", "Labs"];
+    assert.deepEqual(actual, expected);
+  });
+
+  it("returns [2, 3] if the input is [1, 2, 3]", () => {
+    const actual = tail([1, 2, 3]);
+    const expected = [2, 3];
+    assert.deepEqual(actual, expected);
+  });
+
+  it("returns [] if the input is []", () => {
+    const actual = tail([]);
+    const expected = [];
+    assert.deepEqual(actual, expected);
+  });
+
+  it("returns [2] if the input is [1, 2]", () => {
+    const actual = tail([1, 2]);
+    const expected = [2];
+    assert.deepEqual(actual, expected);
+  });
+
+
+});
