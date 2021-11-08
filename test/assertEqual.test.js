555558d05b053d644de019aa265adf1a11a2431d
isissi <isissi_525@hotmail.com>
2021-11-07 21:34:42 -0700

    update

--- test/assertEqual.test.js
+++ test/assertEqual.test.js
@@ -1,13 +1,18 @@
-// const chai = require('chai');
-// const assert = chai.require;
+const chai = require('chai');
+const assert = chai.assert;
 const assertEqual = require("../assertEqual");
 
-// describe('test assertEqual', function() {
-//   it('should', function() {
-
-//   })
-// })
-
 //test code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+describe("#assertEqual", () => {
+  it("returns fasle if the input is 'Lighthouse Labs' and 'Bootcamp'", () => {
+    const result = assertEqual("Lighthouse Labs", "Bootcamp");
+    assert.isFalse(result);
+  });
+
+  it("returns true if the input is 1 and 1", () => {
+    const result = assertEqual(1, 1);
+    assert.isTrue(result);
+  });
+});
