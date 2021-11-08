cfdb48121965b6b0da3b51a40c4275b646b7387a
isissi <isissi_525@hotmail.com>
2021-11-07 20:40:02 -0700

    update

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,8 +3,10 @@ const eqArrays = require('./eqArrays');
 const assertArraysEqual = function (arrOne, arrTwo) {
   if (eqArrays(arrOne, arrTwo) === true) {
     console.log(`✅✅✅ Assertion Passed: ${arrOne} ===  ${arrTwo}`);
+    return true;
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arrOne} !==  ${arrTwo}`);
+    return false;
   }
 };
 
