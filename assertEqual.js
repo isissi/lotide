726fe2efd88b3e6c1ccba7100bf43ba131a0ddaf
isissi <isissi_525@hotmail.com>
2021-11-07 21:07:20 -0700

    update

--- assertEqual.js
+++ assertEqual.js
@@ -1,25 +1,25 @@
 // FUNCTION IMPLEMENTATTION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    if ((typeof actual === 'string') && (typeof expected === 'string')) {
-      console.log(`✅✅✅ Assertion Passed: \"${actual}\" ===  \"${expected}\"`);
+    if (typeof actual === "string" && typeof expected === "string") {
+      console.log(
+        `✅✅✅ Assertion Passed: \"${actual}\" ===  \"${expected}\"`
+      );
     } else {
       console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
     }
-      
-      return true;
+    return true;
   } else {
-    if ((typeof actual === 'string') && (typeof expected === 'string')) {
-      console.log(`🛑🛑🛑 Assertion Failed: \"${actual}\" !==  \"${expected}\"`);
+    if (typeof actual === "string" && typeof expected === "string") {
+      console.log(
+        `🛑🛑🛑 Assertion Failed: \"${actual}\" !==  \"${expected}\"`
+      );
     } else {
       console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
     }
     return false;
   }
 };
-
-// TEST CONDE
-
 
 //export the function
 module.exports = assertEqual;
--- test/eqArrays.test.js
+++ test/eqArrays.test.js
@@ -1,6 +1,25 @@
 const eqArrays = require('../eqArrays');
+const chai = require('chai');
+const assert = chai.assert;
 
 //test code
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+// console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
+describe("#assertEqual", () => {
+  it("returns true if the input is [1, 2, 3] and [1, 2, 3]", () => {
+    const result = eqArrays([1, 2, 3], [1, 2, 3]);
+    assert.isTrue(result);
+  });
+
+  it("returns true if the input is ['1', '2', '3'] and ['1', '2', '3']", () => {
+    const result = eqArrays(["1", "2", "3"], ["1", "2", "3"]);
+    assert.isTrue(result);
+  });
+
+  it("returns false if the input is ['1', '2', '3'] and ['1', '2', 3]", () => {
+    const result = eqArrays(["1", "2", "3"], ["1", "2", 3]);
+    assert.isFalse(result);
+  });
+});
