ee9996e9d31bdfee5a5ff2cbfac2ee3705ee208b
isissi <isissi_525@hotmail.com>
2021-11-12 12:06:50 -0700

    Output value fixed

--- letterPositions.js
+++ letterPositions.js
@@ -1,6 +1,6 @@
 const letterPositions = function(sentence) {
   const results = {};
-  // logic to update results here
+
   for (let i = 0;i < sentence.length; i ++) {
     if (sentence[i] in results) {
       results[sentence[i]].push(i);
